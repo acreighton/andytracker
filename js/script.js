d24db7c5f68b34d565d68fc087301aa9fa42778d
Andy Creighton <acreigh2@gmail.com>
2013-01-27 17:18:47 -0600

    bug fixes; refactoring

--- js/script.js
+++ js/script.js
@@ -25,59 +25,80 @@
 			});
 		}
 	});
+	
 
 	function showData (xml) {
 		$(output).empty();
 
-		var json = $.xml2json(xml);
-		var jsonString = JSON.stringify(json, null, '\t');
+		var json = $.xml2json(xml),
+		    jsonString = JSON.stringify(json, null, '\t'),
+		    eta = json.eta,
+		    etas = [],
+			times = {
+				northBound: [],
+				southBound: []
+			};
+		    
 		console.log(jsonString);
 
-		var eta = json.eta;
-		var len = json.eta.length;
-
-		var times = {
-			northBound: [],
-			southBound: []
-		};
-		
-		for (var i = 0; i < len; i++) {
-
-			//using this date pattern to accommodate Safari:
-			eta[i].arrT = eta[i].arrT.replace(/\D/g, '');
-			eta[i].arrT = new Date(eta[i].arrT.replace(/^(\d{4})(\d\d)(\d\d)(\d\d)(\d\d)(\d\d)$/, '$2/$3/$1 $4:$5:$6'));
-			eta[i].prdt = eta[i].prdt.replace(/\D/g, '');
-			eta[i].prdt = new Date(eta[i].prdt.replace(/^(\d{4})(\d\d)(\d\d)(\d\d)(\d\d)(\d\d)$/, '$2/$3/$1 $4:$5:$6'));
-			eta[i].arrivalTime = eta[i].arrT - eta[i].prdt;
-			eta[i].arrivalTime = Math.floor(eta[i].arrivalTime / 100000);
-
-			//converting isApp to number/boolean
-			eta[i].isApp = Number(eta[i].isApp);
-
-			//looping over operational direction
-			var direction = eta[i].trDr;
-			if (direction == 1) {
-				times.northBound.push(eta[i]);
-				console.log('northBound: ' + times.northBound);
-				console.log(JSON.stringify(times.northBound));
-			} else {
-				times.southBound.push(eta[i]);
-				console.log('southBound: ' + times.southBound);
-				console.log(JSON.stringify(times.southBound));
+		//response sometimes returns eta as object instead of array, so push everything into an array
+		if (typeof eta.length === 'undefined') {
+			etas.push(eta);
+		} else {
+			for (var i = 0; i < eta.length; i++) {
+				etas.push(eta[i]);
 			}
 		}
 
-		var template = '<h2>' + times.northBound[0].stpDe + '</h2>' + 
+		for (var i = 0, len = etas.length; i < len; i++) {
+
+			//using this date pattern to accommodate Safari:
+			etas[i].arrT = etas[i].arrT.replace(/\D/g, '');
+			etas[i].arrT = new Date(etas[i].arrT.replace(/^(\d{4})(\d\d)(\d\d)(\d\d)(\d\d)(\d\d)$/, '$2/$3/$1 $4:$5:$6'));
+			etas[i].prdt = etas[i].prdt.replace(/\D/g, '');
+			etas[i].prdt = new Date(etas[i].prdt.replace(/^(\d{4})(\d\d)(\d\d)(\d\d)(\d\d)(\d\d)$/, '$2/$3/$1 $4:$5:$6'));
+			etas[i].arrivalTime = etas[i].arrT - etas[i].prdt;
+			etas[i].arrivalTime = Math.floor(etas[i].arrivalTime / 100000);
+
+			//converting isApp to number/boolean
+			etas[i].isApp = Number(etas[i].isApp);
+
+			//looping over operational direction
+			var direction = etas[i].trDr;
+			
+			if (direction == 1) {
+				times.northBound.push(etas[i]);
+			} else {
+				times.southBound.push(etas[i]);
+			}
+		}
+
+		function checkSouth () {
+			if (typeof times.southBound[0] === 'undefined') {
+				return '';
+			} else {
+				return '<h2>' + times.southBound[0].stpDe + '</h2>';
+			}
+		}
+
+		function checkNorth () {
+			if (typeof times.northBound[0] === 'undefined') {
+				return '';
+			} else {
+				return '<h2>' + times.northBound[0].stpDe + '</h2>';
+			}
+		}
+
+		var template = checkNorth() + 
 		 			   '{{#northBound}}' +
 				       '<div class="incoming {{rt}}"><p>{{#isApp}}Approaching{{/isApp}}{{^isApp}}{{arrivalTime}} min{{/isApp}}</p></div>' +
 					   '{{/northBound}}' + 
-					   '<h2>' + times.southBound[0].stpDe + '</h2>' + 
+					   checkSouth() + 
 					   '{{#southBound}}' +
 				       '<div class="incoming {{rt}}"><p>{{#isApp}}Approaching{{/isApp}}{{^isApp}}{{arrivalTime}} min{{/isApp}}</p></div>' +
 					   '{{/southBound}}';
 
 		var html = Mustache.to_html(template, times);
-		console.log(html);
 
 		$(output).html(html);
 	}
