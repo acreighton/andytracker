d6dddd330160fdc686caa9718fe550edd588f403
Andy Creighton <acreigh2@gmail.com>
2013-03-06 21:52:20 -0600

    removed jquery dependency

--- js/dropdowns.js
+++ js/dropdowns.js
@@ -1,13 +1,11 @@
 //to build a dropdown, visit one of the CTA mobile sites for a particular line
 //(e.g. http://www.transitchicago.com/mobile/traintrackerstops.aspx?rid=310) and drop this baby in the console.  BOOM
-// This also requires the jQuerify bookmarklet: http://www.learningjquery.com/2009/04/better-stronger-safer-jquerify-bookmarklet
-var divs = $('#ttmobile_stopbuttons').find('div');
-$.each(divs, function (){
-	var stopName = $(this).find('span').text();
-	var id = $(this).find('a').attr('href');
-	var index = id.indexOf('?');
-	var string = id.substr(index + 5);
-	stopName = stopName.replace(/\n/g, '');
-	stopName = stopName.replace(/\s/g, '');
-	console.log('<option value="' + string + '">' + stopName + '</option>');
-});
+(function () {
+	var lines = document.getElementById('ttmobile_stopbuttons').children;
+	for (var i = 0; i < lines.length; i++) {
+		var href     = lines[i].childNodes[1].href,
+		    stopId   = href.substr(href.lastIndexOf('?') + 5),
+		    stopName = lines[i].childNodes[1].children[0].innerText;
+		console.log('<option value="' + stopId + '">' + stopName + '</option>');
+	}
+})();
